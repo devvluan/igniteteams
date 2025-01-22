5aca2fec3f1f44e5ef8404fc8a4e852e5d3698ff
LuanS <luansellera98@gmail.com>
2025-01-22 08:32:13 -0300

    added theme personality

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,21 @@
-import Groups from "@screens/Groups";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import theme from "../../theme";
 
+import Groups from "@screens/Groups";
+
 export default function Index() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
