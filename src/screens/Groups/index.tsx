05ed7add32b7df1990b5efbb835621f10afc8263
LuanS <luansellera98@gmail.com>
2025-01-27 00:19:40 -0300

    component button
    criação do componente Button para reaproveitar na 3 telas da nossa aplicação toda a estrutura e estilização dos botões. Além disso, iremos nos aprofundar um pouco mais no Typescript ao aprender sobre literal types e union types.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,12 +4,13 @@ import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 import { FlatList } from "react-native";
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -25,6 +26,8 @@ export default function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
