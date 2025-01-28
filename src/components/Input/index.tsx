4c20986d95a406316dfd11f142f25f02a752c70b
LuanS <luansellera98@gmail.com>
2025-01-27 23:52:12 -0300

    estilizando cor do place holder pegando COLORS do theme

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,10 @@
+import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
+
 import { Container } from "./styles";
 
-import { TextInputProps } from "react-native";
-
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
